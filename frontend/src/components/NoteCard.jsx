c4723ab67e0187e67d55ad6d4e9ef513fa24a1e2
isaacurba <kpomassiurban@gmail.com>
2025-11-08 00:42:06 +0100

    added api.js file

---
+++ frontend/src/components/NoteCard.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Button } from './ui/button';
+import { card, CardContent, CardHeader } from './ui/card';
+import { Input } from './ui/input';
+import { Textarea } from './ui/textarea';
+const NoteCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NoteCard
--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -12,5 +12,23 @@ const client = axios.create(
 
 // Notes Endpoints
 export const NotesAPI = {
-    
+    list : async (userId) =>{
+        const response = await client.get(`/api/notes`, { params: userId ? { userId } : {} })
+        return response.data;
+    },
+
+    create : async (payload) =>{
+        const response  = await client.post(`/api/notes`, payload)
+        return response.data;
+    },
+
+    update : async (id, payload) =>{
+        const response = await client.put(`/api/notes/${id}`, payload)
+        return response.data;
+    },
+
+    remove : async (id) =>{
+        const response = await client.delete(`/api/notes/${id}`)
+        return response.data;
+    }
 }
